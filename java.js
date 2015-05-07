07b87ca90b08037aa8bd25e58ea8e4cd033fd2f1
Junho Rhee <juno0628@gmail.com>
2015-05-07 14:59:23 -0400

    add

---
+++ java.js
@@ -0,0 +1,40 @@
+console.log("linked");
+console.log("events.js loaded");
+
+var skyblue = function(event) {
+	console.log(event.target);
+	event.target.style.color ="blue";
+	console.log("skyblue stuff");
+}
+
+
+var h1 = document.querySelector('h1');
+h1.addEventListener('mouseover',skyblue);
+
+var h2 = document.querySelector('h2');
+h2.addEventListener('mouseover',skyblue);
+//add makeIt skyblue to the p tag and the anchor tags
+
+var first = document.querySelector('.first');
+first.addEventListener('mouseover', skyblue);
+
+var second = document.querySelector('.second');
+second.addEventListener('mouseover', skyblue);
+
+
+
+var span = document.querySelector('span');
+span.addEventListener('mouseover,')
+var a = document.querySelector('a');
+a.addEventListener('mouseover', skyblue);
+
+
+
+var makeClear = function(event){
+
+	console.log(event.target);
+	document.body.style.color = "black";
+	console.log("cleared");
+}
+
+document.body.addEventListener("dblclick", makeClear);
