0e3bebd98329c6f1030796b4a53c277236992f22
Junho Rhee <juno0628@gmail.com>
2016-12-20 02:11:21 -0500

    added

--- js/javascript.js
+++ js/javascript.js
@@ -1,72 +1,72 @@
 $(document).ready(function(){
 	console.log("added");
-	// var svgImg = d3.select("div.image-box").append("svg")
- //                                     .attr("width", 1600)
- //                                     .attr("height", 300);
+	var svgImg = d3.select("div.image-box").append("svg")
+                                     .attr("width", 1600)
+                                     .attr("height", 300);
 
-	// var svgImgHeight = d3.select("svg").attr("height"); 
-	// var svgImgWidth = d3.select("svg").attr("width");                                     
+	var svgImgHeight = d3.select("svg").attr("height"); 
+	var svgImgWidth = d3.select("svg").attr("width");                                     
 
-	// var svgRect = svgImg.append("rect")
-	// .attr("width", "100%")
-	// .attr("height", "100%")
-	// .attr("fill", "#89DB5F");
+	var svgRect = svgImg.append("rect")
+	.attr("width", "100%")
+	.attr("height", "100%")
+	.attr("fill", "#89DB5F");
 
-	// //Draw the Circle
-
-
-	// for (var i = 0; i < Math.round(Math.random()*500); i++) {
-	// 	var circleArray=[];
-	// 	var circle = svgImg.append("circle")
-	// 	                      .attr("cx", Math.round(Math.random()*1500))
-	// 	                      .attr("cy", Math.round(Math.random()*1500))
-	// 	                     .attr("r", 15)
-	// 	                     .style("fill","#FFFFFF");
-	//   circle.transition().duration(10).attr("r",35);
-	// 	circleArray.push(circle);
-	// };                    
-
-	// d3.select('rect').transition().duration(10000).style("fill","#5AA7F5");
-	// d3.selectAll('circle').transition().duration(11200).attr("r",10);
-
-	// var circleCx = 0;
-	// var circleCy = 0;
+	//Draw the Circle
 
 
-	// for (var i = 0; i < Math.round(Math.random()*500); i++) {
-	// 	var circleArray=[];
-	// 	var circle = svgImg.append("circle")
-	// 	                      .attr("cx", Math.round(Math.random()*1500))
-	// 	                      .attr("cy", Math.round(Math.random()*1500))
-	// 	                     .attr("r", 35)
-	// 	                     .style("fill","#FFFFFF");
-	//   circle.transition().duration(100).attr("r",25);
-	// 	circleArray.push(circle);
-	// };                   
+	for (var i = 0; i < Math.round(Math.random()*500); i++) {
+		var circleArray=[];
+		var circle = svgImg.append("circle")
+		                      .attr("cx", Math.round(Math.random()*1500))
+		                      .attr("cy", Math.round(Math.random()*1500))
+		                     .attr("r", 15)
+		                     .style("fill","#FFFFFF");
+	  circle.transition().duration(10).attr("r",35);
+		circleArray.push(circle);
+	};                    
+
+	d3.select('rect').transition().duration(10000).style("fill","#5AA7F5");
+	d3.selectAll('circle').transition().duration(11200).attr("r",10);
+
+	var circleCx = 0;
+	var circleCy = 0;
 
 
-	// for (var i = 0; i < Math.round(Math.random()*500); i++) {
-	// 	var circleArray=[];
-	// 	var circle = svgImg.append("circle")
-	// 	                      .attr("cx", Math.round(Math.random()*1500))
-	// 	                      .attr("cy", Math.round(Math.random()*1500))
-	// 	                     .attr("r", 55)
-	// 	                     .style("fill","#FFFFFF");
-	//   circle.transition().duration(100).attr("r",15);
-	// 	circleArray.push(circle);
-	// };  
+	for (var i = 0; i < Math.round(Math.random()*500); i++) {
+		var circleArray=[];
+		var circle = svgImg.append("circle")
+		                      .attr("cx", Math.round(Math.random()*1500))
+		                      .attr("cy", Math.round(Math.random()*1500))
+		                     .attr("r", 35)
+		                     .style("fill","#FFFFFF");
+	  circle.transition().duration(100).attr("r",25);
+		circleArray.push(circle);
+	};                   
 
 
-	// for (var i = 0; i < Math.round(Math.random()*500); i++) {
-	// 	var circleArray=[];
-	// 	var circle = svgImg.append("circle")
-	// 	                      .attr("cx", Math.round(Math.random()*1500))
-	// 	                      .attr("cy", Math.round(Math.random()*1500))
-	// 	                     .attr("r", 95)
-	// 	                     .style("fill","#FFFFFF");
-	//   circle.transition().duration(100).attr("r",20);
-	// 	circleArray.push(circle);
-	// };          
+	for (var i = 0; i < Math.round(Math.random()*500); i++) {
+		var circleArray=[];
+		var circle = svgImg.append("circle")
+		                      .attr("cx", Math.round(Math.random()*1500))
+		                      .attr("cy", Math.round(Math.random()*1500))
+		                     .attr("r", 55)
+		                     .style("fill","#FFFFFF");
+	  circle.transition().duration(100).attr("r",15);
+		circleArray.push(circle);
+	};  
+
+
+	for (var i = 0; i < Math.round(Math.random()*500); i++) {
+		var circleArray=[];
+		var circle = svgImg.append("circle")
+		                      .attr("cx", Math.round(Math.random()*1500))
+		                      .attr("cy", Math.round(Math.random()*1500))
+		                     .attr("r", 95)
+		                     .style("fill","#FFFFFF");
+	  circle.transition().duration(100).attr("r",20);
+		circleArray.push(circle);
+	};          
 
 
 
